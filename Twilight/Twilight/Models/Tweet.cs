4d44eabca4c0cb7af7ed2bd4cf8658636d021f99
Yamagn <syankenpon@gmail.com>
2018-11-05 02:32:34 +0900

    TLの取得、表示

--- Twilight/Twilight/Models/Tweet.cs
+++ Twilight/Twilight/Models/Tweet.cs
@@ -4,16 +4,9 @@ namespace Twilight.Models
 {
     class Tweet
     {
-        private readonly Tokens _token;
         public Tweet()
         {
-            // APIにアクセスするためのトークン
-            _token = Tokens.Create(
-                Key.consumerKey,
-                Key.consumerSecret,
-                Key.accessToken,
-                Key.accessTokenSecret
-            );
+            
         }
     }
 }
--- Twilight/Twilight/Pages/homePage.xaml.cs
+++ Twilight/Twilight/Pages/homePage.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Twilight.Models;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -22,7 +21,20 @@ namespace Twilight.Pages
             {
                 await Navigation.PushAsync(new authPage());
             }
-            
+            else
+            {
+                timeLine.ItemsSource = await App.tokens.Statuses.HomeTimelineAsync(count => 50);
+            }
+        }
+
+        async void tweet_Selected(object sender, ItemTappedEventArgs e)
+        {
+
+        }
+
+        async void refreshTL(object sender, EventArgs e)
+        {
+
         }
 
         private async void newTweet_Clicked(object sender, EventArgs e)
